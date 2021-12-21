bc4f35aeb8f4e4f02127b31929f13eb00da0a387
cahnakalmon <38774854+cahnakalmon@users.noreply.github.com>
2021-12-21 15:16:13 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+st.title("Hello streamlit")
+st.subheader("Welcome to my streamlit adventure")
+st.text("something cool was created here")
+
